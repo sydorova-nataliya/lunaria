003438236bc14d1c4b2be0626e592b3c6ad1cc04
sydorova-nataliya <nsidorova200363@gmail.com>
2023-01-11 23:56:18 +0300

    added section with contacts and footer

--- dist/js/jquery.main.js
+++ dist/js/jquery.main.js
@@ -1,15 +1,19 @@
 $( document ).ready(function() {
-    // $('.list-lang__link').click(function(e){
-    //     $('.list-lang__link').removeClass('active');
-    //     $(this).addClass('active');
-    // })
-    // $('.navigation__link').click(function(e){
-    //     $('.navigation__link').removeClass('active');
-    //     $(this).addClass('active');
-    // })
-    // $('.navigation__btn').click(function(e){
-    //     $('.navigation').toggleClass('active');
-    // })
+    
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn();
+      } else {
+      $('#scroll').fadeOut();
+      }
+      });
+       
+      $('#scroll').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+      });
+       
+
     $(function() {
         $('.tabs__btn').click(function(e){
             e.preventDefault();
